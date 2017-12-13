4e1c3144448f8aff893cd214126e12c5a864ac9d
Ignacio (nachinius) Peixoto <ignacio.peixoto@gmail.com>
2017-12-12 21:20:32 -0300

    add successor and fix member

--- src/main/scala/com/nachinius/vanEmdeBoas/SuccessorPredecessor.scala
+++ src/main/scala/com/nachinius/vanEmdeBoas/SuccessorPredecessor.scala
@@ -1,26 +1,23 @@
 package com.nachinius.vanEmdeBoas
 
 import scala.collection.mutable
-import scala.collection.mutable.Map
+import scala.language.higherKinds
 
 trait SuccessorPredecessor {
 
   type T
   type Set
   def successor(x: T): Option[T]
-  def predecessor(x: T): Option[T]
+  def predecessor(x: T): Option[T] = successor(x) // @TODO
 }
 
-trait Membership {
-  type T
-  type Set
-  def insert(x: T): Set
+trait Membership[T] {
+  def insert(x: T): Membership[T]
   def member(x: T): Boolean
 }
 
 object vanEmdeBoas {
   def apply[T](bits: Int): vanEmdeBoas = {
-    println(s"creating vEB of $bits bits")
     if(bits == 1) new vEBSmall() else new vEB(bits)
   }
 }
@@ -28,35 +25,50 @@ object vanEmdeBoas {
 case class Upper(value: Int) extends AnyVal
 case class Lower(value: Int) extends AnyVal
 
-abstract class vanEmdeBoas extends Membership {
+abstract class vanEmdeBoas extends Membership[Int] with Traversable[Int] with SuccessorPredecessor {
   type T = Int
   var min: Option[Int] = None
   var max: Option[Int] = None
   type Set = vanEmdeBoas
+  val maxNumber: Int
 }
 
 class vEB(bits: Int) extends vanEmdeBoas {
+  val maxNumber = (1 << bits)-1
+  val minNumber = 0
   val halfbits: Int = bits / 2
   val lowerbits: Int = bits - bits / 2
   require(halfbits + lowerbits == bits)
+  require(bits>1)
+
+  override def foreach[U](f: T => U): Unit = {
+    min.fold() { min =>
+      f(min)
+      cluster.foreach {
+        case (z @ Upper(v) , boas: Set) =>
+          boas.foreach(f)
+      }
+    }
+  }
 
   val summary: vanEmdeBoas = vanEmdeBoas(halfbits)
   val cluster: mutable.Map[Upper,vanEmdeBoas] = mutable.Map()
 
   def getUpper: Int => Upper = x => Upper(x >>> halfbits)
-  def getLower: Int => Lower = x => Lower(x & (1 << halfbits - 1))
+  def getLower: Int => Lower = x => Lower(x & ((1 << halfbits) - 1))
   def expr: Int => (Upper,Lower) = x => (getUpper(x),getLower(x))
+  def toNumber(c: Upper, l: Lower): Int = (c.value << halfbits) | l.value
 
   override def insert(x: T): Set = {
-    println(s"inserting $x")
     if(min.isEmpty) { // vEb is empty, very few to do and no recursions
       min = Some(x)
       max = Some(x)
     } else {
 
-      var minimum = min.get
+      val minimum = min.get
       var y: T = x
       if(y < minimum) {
+        // swap x <-> minm
         y = minimum
         min = Some(x)
       }
@@ -75,22 +87,60 @@ class vEB(bits: Int) extends vanEmdeBoas {
     this
   }
 
-  override def member(x: Int): Boolean = {
-    min match {
+  override def member(x: Int): Boolean = min match {
       case None => false // no min, so is completely empty
-      case Some(mn) => if(x == mn || max.contains(x)) true else {
-        if(max.get > x) false else {
+      case Some(mn) =>
+        if(x == mn || max.contains(x)) true
+        else if(max.nonEmpty && max.get < x) false
+        else {
           val (c, l) = expr(x)
           cluster.get(c).fold(false)((b: vanEmdeBoas) => b.member(l.value))
         }
       }
-    }
+
+
+  // SuccessorPredecessor
+  override def successor(x: Int): Option[Int] = min match {
+    case None => None // no min, empty veb, impossible successor
+    case Some(m) if x<m => min // trivial successor
+    case _ => // need to look inside
+      val (c, l) = expr(x)
+      getSuccessorInsideCluster(c.value,l.value) orElse getSuccessorFromSummary(c.value,l.value)
   }
+
+  def getSuccessorInsideCluster(upper: Int, lower: Int): Option[Int] = for {
+    cl <- cluster.get(Upper(upper))
+    max: Int <- cl.max if(lower< max)
+    next <- cl.successor(lower)
+  } yield toNumber(Upper(upper),Lower(next))
+
+  def getSuccessorFromSummary(upper: Int, lower: Int): Option[Int] = for {
+    successorClusterNumber <- summary.successor(upper)
+    clusterOfSuccessor <- cluster.get(Upper(successorClusterNumber))
+    successorLower <- clusterOfSuccessor.min
+  } yield toNumber(Upper(successorClusterNumber),Lower(successorLower))
+
+
 }
 
 
-class vEBSmall extends vanEmdeBoas {
-   override def member(x: Int): Boolean = {
+class vEBSmall() extends vanEmdeBoas {
+
+  override val maxNumber: Int = 1
+
+  override def successor(x: Int): Option[Int] =
+    if(x == 0 && max.contains(1)) max else None
+
+  override def foreach[U](f: T => U): Unit = {
+    if(min.isDefined) {
+      f(min)
+      if(max.isDefined && max.get != min.get) {
+        f(max)
+      }
+    }
+  }
+
+  override def member(x: Int): Boolean = {
     if(x==0) min.contains(0)
     else if(x==1) max.contains(1)
     else false
--- src/test/scala/com/nachinius/vanEmdeBoas/vEBTest.scala
+++ src/test/scala/com/nachinius/vanEmdeBoas/vEBTest.scala
@@ -1,8 +1,10 @@
 package com.nachinius.vanEmdeBoas
 
-import org.scalatest.FreeSpec
+import org.scalatest.{FreeSpec, Matchers}
 
-class vEBTest extends FreeSpec {
+import scala.util.Random
+
+class vEBTest extends FreeSpec with Matchers {
 
   "Methods tests" - {
     "insert" in {
@@ -10,7 +12,41 @@ class vEBTest extends FreeSpec {
       assert(a.insert(3).member(3))
       assert(a.insert(4).member(3))
       assert(a.member(4))
-      assert(a.insert(1 << 7  - 1).member( 1<<7-1))
+      val i = (1 << 7) - 1
+      assert(a.insert(i).member( i))
+    }
+    "successor" in {
+      val veb = vanEmdeBoas(8)
+      val seed = 1
+      val rnd = new Random(seed)
+      val original = List(2,5,8,12,16,20,24,30,31,32,33,50,54)
+      val lst = rnd.shuffle(original)
+      // test
+      lst foreach veb.insert
+      val results = original map veb.successor
+      val expectedSuccessor = original.tail.map(Some(_)) :+ None
+
+      veb.successor(2) shouldBe Some(5)
+      veb.successor(5) shouldBe Some(8)
+      veb.successor(66) shouldBe  None
+      veb.successor(0) shouldBe Some(2)
+      veb.successor(21) shouldBe Some(24)
+
+      results shouldBe expectedSuccessor
+
+      (original map veb.member) shouldBe original.map(_=>true)
+    }
+    "member" in {
+      val veb = vanEmdeBoas(8)
+      val seed = 1
+      val rnd = new Random(seed)
+
+      val original = (1 to 1000).map(_ => rnd.nextInt(veb.maxNumber+1))
+      val lst = rnd.shuffle(original)
+      lst foreach veb.insert
+
+      (original map veb.member) should not contain (false)
+      (0 to 100).toList.diff(original).map(veb.member) should not contain (true)
     }
 
 
